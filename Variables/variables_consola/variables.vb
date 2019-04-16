16c67220e068ac5dd10ed1e92ccc9c276f50d3a3
Gonzalo Techeira <gnxa95@hotmail.com>
2019-04-15 23:57:21 -0300

    ...

---
+++ Variables/variables_consola/variables.vb
@@ -0,0 +1,17 @@
+Imports System
+
+Module Program
+    Sub Main()
+        Dim nombre, apellido As String
+        Dim fecha_nacimiendo As Date
+
+        Console.WriteLine("Ingrese Nombre")
+        nombre = Console.ReadLine()
+        Console.WriteLine("Ingrese Apellido")
+        apellido = Console.ReadLine()
+        Console.WriteLine("Ingrese Fecha de Nacimiento")
+        fecha_nacimiendo = Console.ReadLine()
+
+        Console.WriteLine(apellido & " " & nombre & " " & "Nacio el :" & fecha_nacimiendo)
+    End Sub
+End Module
