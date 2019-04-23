52e954852b84d2726ed2d340aecc6f6d78e0f8cf
Gonzalo Techeira <gnxa95@hotmail.com>
2019-04-22 21:39:57 -0300

    ...

---
+++ Variables/aritmetica/aritmetica.vb
@@ -0,0 +1,14 @@
+Imports System
+
+Module Program
+    Sub Main()
+        Dim Entero1 As Integer = 2
+        Dim Entero2 As Integer = 3
+        Dim Decimal1 As Decimal = 1.5
+        Dim Decimal2 As Decimal = 3.5
+
+        Console.WriteLine("Entero mas Entero: " & Entero1 + Entero2)
+        Console.WriteLine("Decimal mas Decimal: " & Decimal1 + Decimal2)
+        Console.WriteLine("Entero mas Decimal: " & Entero1 + Decimal1)
+    End Sub
+End Module
--- Variables/concatenacion/concatenacion.vb
+++ Variables/concatenacion/concatenacion.vb
@@ -3,10 +3,23 @@ Imports System
 Module Program
     Sub Main()
         Dim NombreEmpresa As String = "NoManzana"
-        Dim NombreCalle As String = ""
-        Dim AlturaCalle As UShort = "2200"
-        Dim FechaActividades As Date = "25/04/2019"
+        Dim NombreCalle As String = "Rademacher"
+        Dim AlturaCalle As UShort = 2200
+        Dim FechaActividades As Date = #05/04/2019#
 
+        'Concatenacion Implicita
+        Dim altura As String = AlturaCalle
+        Dim fecha As String = FechaActividades
+        Console.WriteLine("El nombre de la Empresa es: " & NombreEmpresa)
+        Console.WriteLine("El Nombre de la calle es: " & NombreCalle)
+        Console.WriteLine("La altura es de: " & altura)
+        Console.WriteLine("La fecha de actividades: " & fecha)
+
+        'Concatenacion Explicita
+        Console.WriteLine("El nombre de la Empresa es: " & NombreEmpresa)
+        Console.WriteLine("El Nombre de la calle es: " & NombreCalle)
+        Console.WriteLine("La altura es de: " & Convert.ToString(AlturaCalle))
+        Console.WriteLine("La fecha de actividades: " & Convert.ToString(FechaActividades))
 
 
     End Sub
